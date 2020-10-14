0244bdb9da6ca8cbcc821bdcf5abe56f06332e22
Tanner <tdyer@hawk.iit.edu>
2020-10-14 12:27:37 -0500

    Created stock class

---
+++ app/src/main/java/com/example/stockwatcher/Stock.java
@@ -0,0 +1,52 @@
+package com.example.stockwatcher;
+
+public class Stock {
+
+    String symbol;
+    String company;
+    double price;
+    double priceChange;
+    double changePercentage;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPriceChange() {
+        return priceChange;
+    }
+
+    public void setPriceChange(double priceChange) {
+        this.priceChange = priceChange;
+    }
+
+    public double getChangePercentage() {
+        return changePercentage;
+    }
+
+    public void setChangePercentage(double changePercentage) {
+        this.changePercentage = changePercentage;
+    }
+
+
+}
